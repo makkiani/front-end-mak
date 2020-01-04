0a93618cced0ed376cb8c3a85567f5581dbce912
makkiani <59272039+makkiani@users.noreply.github.com>
2020-01-03 21:10:02 -0600

    Add files via upload

---
+++ java.js
@@ -0,0 +1,13 @@
+function printName() {
+  alert("Kiani");
+}
+function agesexloc() {
+  console.log(50, "M", "Austin");
+}
+
+let runrate = 0;
+
+function calRunrate(runs, balls) {
+  runrate = runs / balls;
+  alert(runrate);
+}
